4929309bea7350a3719e113e382eddd66fe6cf8b
MuriloFontes21 <124307575+MuriloFontes21@users.noreply.github.com>
2023-02-23 19:14:57 -0300

    Add files via upload

---
+++ JavaApplication1M/src/TextFieldFrame.java
@@ -0,0 +1,20 @@
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JOptionPane;
+        
+public class TextFieldFrame extends JFrame
+{
+    private JTextField1 textField1; // campo de texto com tamanho configurado
+    private JTextField textField2;  // campo de texto construído com texto
+    private JTextField textField3;  // campo de texto com texto e tamanho
+    private JPasswordField passwordField; // campo de senha com texto
+    
+    // construtor TextFieldFrame adiciona JTextFields a Jframe
+    public TextFieldFrame ()
+            
+            
+}
